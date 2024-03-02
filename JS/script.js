0700d82fcfd744037b3be010978215319329f494
Aqib Ali Kalwar <aqibalikalwar1@gmail.com>
2024-03-02 15:10:07 +0500

    Update Index file

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
-if (
-  window.location.href.includes("index.html") ||
-  window.location.href === "https://aqibali25.github.io/ezitech-intern-test/"
-) {
-  checkLoginStatus();
-}
-function checkLoginStatus() {
-  const isLoggedIn = sessionStorage.getItem("user");
-  if (!isLoggedIn) {
-    window.location.href = "login.html";
-  }
-}
+// if (
+//   window.location.href.includes("index.html") ||
+//   window.location.href === "https://aqibali25.github.io/ezitech-intern-test/"
+// ) {
+//   checkLoginStatus();
+// }
+// function checkLoginStatus() {
+//   const isLoggedIn = sessionStorage.getItem("user");
+//   if (!isLoggedIn) {
+//     window.location.href = "login.html";
+//   }
+// }
 function logout() {
   sessionStorage.removeItem("user");
   window.location.href = "login.html";
@@ -17,10 +17,14 @@ function logout() {
 var dropdown = document.getElementById("dropdown");
 
 function show() {
-  if (dropdown.style.top === "70px") {
-    dropdown.style.top = "-500px";
+  if (dropdown.style.top === "-9px") {
+    dropdown.style.top = "105%";
+    dropdown.style.height = "100px";
+    dropdown.style.borderTopRightRadius = "0";
   } else {
-    dropdown.style.top = "70px";
+    dropdown.style.top = "-9px";
+    dropdown.style.height = "10px";
+    dropdown.style.borderTopRightRadius = "30px";
   }
 }
 var menuBtn = document.getElementById("menu-btn");
@@ -37,3 +41,13 @@ menuBtn.addEventListener("click", function () {
   closeBtn.style.left = "15px";
   navLinks.style.left = "0";
 });
+function addClass(element) {
+  // Remove "active" class from all divs
+  var allDivs = document.querySelectorAll(".sideBar div");
+  allDivs.forEach(function (div) {
+    div.classList.remove("active");
+  });
+
+  // Add "active" class to the clicked div
+  element.classList.add("active");
+}
